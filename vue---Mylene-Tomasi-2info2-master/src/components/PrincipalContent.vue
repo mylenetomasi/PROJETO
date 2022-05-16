a0da9977fb19f67fb362547119e45041dfe38fdc
mylenetomasi <99373744+mylenetomasi@users.noreply.github.com>
2022-05-16 09:35:59 -0300

    Add files via upload

---
+++ vue---Mylene-Tomasi-2info2-master/src/components/PrincipalContent.vue
@@ -0,0 +1,22 @@
+<script>
+import NovoBloco from "./NovoBloco.vue";
+export default {
+  components: { NovoBloco },
+  data() {
+    return {
+      professores: ["", "", ""],
+    };
+  },
+};
+</script>
+<template>
+  <main id="content">
+    <NovoBloco titulo="" />
+    <NovoBloco titulo="" />
+    <NovoBloco class="" />
+    <NovoBloco class="" />
+    <NovoBloco class="" />
+    <NovoBloco />
+  </main>
+</template>
+<style></style>
